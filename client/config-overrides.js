b83c9c163e3968969ffe681b210c73aea097073d
Nathan Zukoff <nzukoff@gmail.com>
2019-03-01 00:37:44 -0800

    update config-overrides

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,8 +2,10 @@ const rewireDefinePlugin = require('react-app-rewire-define-plugin')
 
 module.exports = function override(config, env) {
     config = rewireDefinePlugin(config, env, {
-      REACT_APP_MP_CHANNEL_SURF: JSON.stringify(process.env.REACT_APP_MP_CHANNEL_SURF),
-      REACT_APP_YT_KEY: JSON.stringify(process.env.REACT_APP_YT_KEY)
+      env: {
+        REACT_APP_MP_CHANNEL_SURF: JSON.stringify(process.env.REACT_APP_MP_CHANNEL_SURF),
+        REACT_APP_YT_KEY: JSON.stringify(process.env.REACT_APP_YT_KEY)
+      }
       })
     return config;
   }
